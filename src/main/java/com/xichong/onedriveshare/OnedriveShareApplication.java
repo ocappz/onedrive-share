403cb482a740231d3a8e0675870d975846aa5624
ocapp <ocapp@sina.cn>
2021-12-16 13:02:27 +0800

    init

---
+++ src/main/java/com/xichong/onedriveshare/OnedriveShareApplication.java
@@ -0,0 +1,13 @@
+package com.xichong.onedriveshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnedriveShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnedriveShareApplication.class, args);
+	}
+
+}
